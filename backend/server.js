c839b48bc06c03e8eddfcff2a90654371fef2454
minghanminghan <andrewjiang6789@gmail.com>
2024-10-06 15:38:41 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,33 @@
 // imports
 const express = require("express");
-const app = express();
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+
+let users = {};
+
+app.use((req, res, next) => {
+    const date = new Date();
+    console.log(date.toISOString(), req.url);
+    next();
+})
 
 app.get('/', (req, res) => {
-    //res.setHeader('Content-Type', 'text/html');
     res.send('<h>hello world!</h>');
 })
+
+app.post('/', (req, res) => { // body parser is fucked, res is fucked
+    const id = Object.keys(users).length;
+    //for(let i of req.body) {
+    //    console.log(i);
+    //}
+    console.log(req.body)
+    res.status(200).send({response: 'received'});
+})
+
+
+
 
 
 app.listen(3000, (error) => {
